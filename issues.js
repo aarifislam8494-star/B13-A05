2b7f2ffcee962513eafc2565ec5c4331165ced78
aarifislam8494-star <aarifislam8494@gmail.com>
2026-03-09 13:19:53 +0600

    added model

--- issues.js
+++ issues.js
@@ -3,6 +3,7 @@ const allBtn = document.getElementById("allbtn");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 const loadingSpinner = document.getElementById("loadingSpinner");
+const cardDetailsModal = document.getElementById("card_modal");
 
 const loadIssues = (status = 'all') => {
     showSpinner();
@@ -42,18 +43,17 @@ const displayIssues = (issues) => {
         // console.log(issue);
         const problem = document.createElement('div');
         problem.innerHTML = `
-         <div style="${getBorder(issue.status)}" class="p-[20px] max-w-[350px] h-[100%] space-y-6 bg-white shadow-[0_4px_10px_rgba(0,0,0,0.1)]">
+         <div style="${getBorder(issue.status)}" onclick="openModal(${issue.id})" class="cursor-pointer p-[20px] max-w-[350px] h-[100%] space-y-6 bg-white shadow-[0_4px_10px_rgba(0,0,0,0.1)]">
             <span style="${getPriorityColor(issue.priority)}"class="py-[15px] px-[25px] rounded-lg font-semibold">${issue.priority}</span>
             <h3 class="font-semibold text-xl mt-[25px]">${issue.title}</h3>
-            <p class="text-[#64748B]">${issue.description}</p>
+            <p class="text-[#64748B] line-clamp-2">${issue.description}</p>
             <div class="mt-[12px] flex gap-[12px] overflow-hidden">
-                <span class="rounded-lg  px-[10px] py-[8px] text-[red] bg-[#feecec]">${issue.labels[0]}</span>
-                ${issue.labels[1] ? `<span class="px-[10px] rounded-lg py-[8px] text-[#91918e] bg-[#fff6db]">${issue.labels[1]}</span>` : ''}
+                <span class="rounded-lg  px-[10px] h-fit py-[8px] text-[red] bg-[#feecec]">${issue.labels[0]}</span>
+                ${issue.labels[1] ? `<span class="px-[10px] h-fit rounded-lg py-[8px] text-[#91918e] bg-[#fff6db]">${issue.labels[1]}</span>` : ''}
+                </div>
             <div style ="border-top: 1px solid #64748B">
                 <p class="text-[#64748B] text-[17px] mt-[10px]">#Author by ${issue.author}</p>
-                ${issue.assignee ? `<span class="text-[#64748B] text-[17px]">#Assignee by ${issue.assignee}</span>` : ''}
-                <p class="text-[#64748B] text-[17px]">#CreateAt ${issue.createdAt}</p>
-                <p class="text-[#64748B] text-[17px]">#UpdateAt ${issue.updatedAt}</p>
+                <p class="text-[#64748B] text-[17px]">#CreateAt ${new Date(issue.createdAt).toLocaleDateString('en-GB')}</p>
             </div>
         </div>
         `
@@ -68,7 +68,7 @@ function hideSpinner() {
     loadingSpinner.classList.add('hidden')
 }
 
-document.querySelectorAll('.btn:not(.btn-new)').forEach(btn => {
+document.querySelectorAll('.button:not(.btn-new)').forEach(btn => {
     btn.addEventListener('click', () => {
         document.querySelectorAll('.btn:not(.btn-new)').forEach(b => b.classList.replace('btn-primary', 'btn-outline'));
         btn.classList.replace('btn-outline', 'btn-primary');
@@ -76,4 +76,30 @@ document.querySelectorAll('.btn:not(.btn-new)').forEach(btn => {
         loadIssues(status);
     });
 });
+
+function openModal(id) {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    fetch(url).then((res) => res.json()).then((data) => {
+        const issuE = data.data;
+        document.getElementById('modal_title').innerText = issuE.title
+        document.getElementById('modal_status').innerText = issuE.status;
+        document.getElementById('modal_author').innerText = issuE.author;
+        document.getElementById('update_date').innerText = new Date(issuE.updatedAt).toLocaleDateString('en-GB');
+        document.getElementById('modal_description').innerText = issuE.description;
+        const assigneeEl = document.getElementById('modal_assignee');
+        if (issuE.assignee) {
+            assigneeEl.innerText = issuE.assignee;
+            assigneeEl.parentElement.style.display = 'block';
+        } else {
+            assigneeEl.innerText = "No Assignee Found"
+        }
+
+        const priorityElement = document.getElementById('modal_priority');
+        priorityElement.innerText = issuE.priority;
+        priorityElement.setAttribute('style', getPriorityColor(issuE.priority));
+
+    });
+    cardDetailsModal.showModal();
+    // console.log('Open Modal',id);
+}
 loadIssues();
